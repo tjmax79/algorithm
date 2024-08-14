2dc077d79310efae2a72cd900c6b016e750d6ae6
tjmax79 <ogidantunji@gmail.com>
2024-08-14 22:08:38 +0100

    algorithm2

--- algorithm.js
+++ algorithm.js
@@ -1,47 +1,24 @@
-function analyzeSentence(sentence) {
-    let length = 0;
-    let wordCount = 0;
-    let vowelCount = 0;
-    let inWord = false;
+function insertionSort(arr) {
+    // Iterate over the array starting from the second element
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i]; // The element to be inserted into the sorted portion
+        let j = i - 1; // The index of the last element in the sorted portion
 
-    for (let i = 0; i < sentence.length; i++) {
-        let char = sentence[i];
-        
-        if (char === '.') {
-            break;
+        // Move elements of arr[0..i-1] that are greater than key to one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j]; // Shift element to the right
+            j--; // Move to the next element in the sorted portion
         }
-        
-        length++;
-        
-        if ('aeiouAEIOU'.includes(char)) {
-            vowelCount++;
-        }
-        
-        if (char === ' ') {
-            if (inWord) {
-                wordCount++;
-                inWord = false;
-            }
-        } else {
-            inWord = true;
-        }
+
+        // Place the key in its correct position
+        arr[j + 1] = key;
     }
-    
-    // Final check to count the last word
-    if (inWord) {
-        wordCount++;
-    }
-    
-    return {
-        length: length,
-        wordCount: wordCount,
-        vowelCount: vowelCount
-    };
+
+    return arr;
 }
 
 // Example usage:
-const sentence = "This is an example sentence.";
-const result = analyzeSentence(sentence);
-console.log("Length:", result.length);
-console.log("Number of words:", result.wordCount);
-console.log("Number of vowels:", result.vowelCount);
+const array = [5, 2, 9, 1, 5, 6];
+console.log("Original array:", array);
+const sortedArray = insertionSort(array);
+console.log("Sorted array:", sortedArray);
